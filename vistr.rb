4fea8fc68101dbd3ca498d6650ccac1332f6533f
Jordan Byron <byron@MacPro.local>
2009-08-27 10:32:48 -0400

    Use SSL and post to the right page

--- vistr.rb
+++ vistr.rb
@@ -1,32 +1,26 @@
-#
-#  vista.rb
-#  
-#
-#  Created by Jordan Byron on 8/26/09.
-#  Copyright (c) 2009 Duck Soup Software. All rights reserved.
-#
-
 require 'rubygems'
 require 'uri'
 require 'net/http'
+require 'net/https'
 # require 'open-uri'
 require 'hpricot'
 
 class Vistr
 
   def initialize
-    @uri = URI.parse("http://vista.csus.ct.edu/webct")
+    @uri = URI.parse("https://vista.csus.ct.edu/webct")
   end
   
   def login username, password
     @username = username
-    result = post("logonHelp.dowebct", :loginDisplay => true, 
-    :glcid => "URN:X-WEBCT-VISTA-V1:21fcd19f-0a62-04bb-0011-18e6329eb151",
-    :insID => "129143011",
-    :gotoid => "null",
-    :insName => "Southern Connecticut State University",
-    :webctid => username,
-    :password => password)
+    result = post("authenticateUser.dowebct", 'loginDisplay' => 'true', 
+    'glcid' => "URN:X-WEBCT-VISTA-V1:21fcd19f-0a62-04bb-0011-18e6329eb151",
+    'insID' => "129143011",
+    'gotoid' => "null",
+    'insName' => "Southern Connecticut State University",
+    'timeZoneOffset' => '4',
+    'webctid' => "#{username}",
+    'password' => "#{password}")
   end
 
   def url_for(path)
@@ -34,7 +28,17 @@ class Vistr
   end
 
   def post(path, data = {})
-    Net::HTTP.post_form URI.parse(url_for(path)), data
+    url = URI.parse(url_for(path))
+    
+    req = Net::HTTP::Post.new url.path
+    req.set_form_data data
+    
+    http = Net::HTTP.new(url.host,url.port)
+    http.use_ssl = true
+    
+    result = http.request(req)
+    
+    result.body
   end
 
   def get(path = nil, options = {})
