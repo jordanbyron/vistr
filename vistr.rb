3ec8589775574dbd11c1262ace2d3992a8f0e0cd
Jordan Byron <jordan@ducksoupsoftware.com>
2009-09-29 20:29:30 -0400

    Added assignments list

--- vistr.rb
+++ vistr.rb
@@ -1,17 +1,19 @@
 require 'rubygems'
 require 'mechanize'
 require 'logger'
+#require 'vistr/course'
 
 class Vistr
   
   def initialize
     @url = "vista.csus.ct.edu/webct"
-    @agent = WWW::Mechanize.new { |a| a.log = Logger.new("vistr.log") }
+    @agent = WWW::Mechanize.new { |agent| 
+      agent.log = Logger.new("vistr.log")
+      agent.follow_meta_refresh = true
+    }
   end
   
-  def login username, password
-    # FIXME: Verify this is using SSL
-    
+  def login username, password    
     page = @agent.post("https://#{@url}/authenticateUser.dowebct",
                       'glcid'           => "URN:X-WEBCT-VISTA-V1:21fcd19f-0a62-04bb-0011-18e6329eb151",
                       'insId'           => "129143011",
@@ -34,8 +36,39 @@ class Vistr
     classes
     
   end
-end
+  
+  def assignments
+    courses = course_list
+    assignments = Array.new
+    
+    courses.each do |course|
+      assignments << course_assignments(course)
+    end
+    
+    assignments.compact
+  end
+  
+  def course_assignments(course)
+    page = @agent.get "http://#{@url}/populateMyWebCT.dowebct"
+    course_regex = Regexp.new(course)
+    course_id_regex = /\/webct\/urw\/(\S*)\//
+    course_id = ""
+    assignments = Array.new
+    
+    page.search("//ul[@class='courselist']/li/a").each do |link|
+      course_id = course_id_regex.match(link['href']).captures[0] if link.content[course_regex] && course_id_regex.match(link['href'])
+    end
 
+    course_page = @agent.get "http://#{@url}/urw/#{course_id}/studentViewSubmissions.dowebct?viewType=INBOX"
+    
+    course_page.search("//table[@class='inventorytable']//a[@title='Edit']").each do |link|
+      due = /\(Due([^\)]*)/.match(link.parent.parent.search("//div[@class='descript']").first.content.gsub("\n","")).captures[0]
+      assignments << [link.content.strip, due] if link.content.length > 0
+    end
+    
+    assignments.compact
+  end
+end
 
 ## FIXME: Use aliasing rather than blowing over the existing method
 
